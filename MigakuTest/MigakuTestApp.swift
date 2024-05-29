1409dc4c3f3f925801943c3cb190ba93862a530d
Landon Mann <mann.landon@gmail.com>
2024-05-29 09:51:21 +0900

    Initial Commit

---
+++ MigakuTest/MigakuTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigakuTestApp.swift
+//  MigakuTest
+//
+//  Created by Landon Mann on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MigakuTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
